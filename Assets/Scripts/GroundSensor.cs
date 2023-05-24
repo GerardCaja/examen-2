f9862297f353430c60aa960b55b138b032002fa8
gerard <gerardsori@gmail.com>
2023-05-24 10:16:21 +0200

    examen 3r trimestre

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    private PlayerController controller;
+    public bool isGrounded;
+    
+    
+
+    SFXManager sfxManager;
+    SoundManager soundManager;
+    GameManager gameManager;
+
+    void Awake()
+    {
+        controller = GetComponentInParent<PlayerController>();
+      
+        sfxManager = GameObject.Find("SFXManager").GetComponent<SFXManager>();
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+        }
+        else if(other.gameObject.layer == 6)
+        {
+            Debug.Log("Goomba muerto");
+
+            sfxManager.GoombaDeath();
+            
+            Enemy goomba = other.gameObject.GetComponent<Enemy>();
+            goomba.Die();
+
+        }
+
+
+        if(other.gameObject.tag == "DeadZone")
+        {
+            Debug.Log("Estoy muerto");
+
+            soundManager.StopBGM();
+            sfxManager.MarioDeath();
+            gameManager.GameOver();
+            //SceneManager.LoadScene()
+        }
+
+        
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+            controller.anim.SetBool("IsJumping", false);
+        }  
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+        }
+
+        
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = true;
+            controller.anim.SetBool("IsJumping", false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = false;
+        }
+
+        
+        if(other.gameObject.layer == 3)
+        {
+            isGrounded = false;
+            controller.anim.SetBool("IsJumping", true);
+        }
+    }
+}
